15fa338902f60864f0196ffb9e651fa0a623a7cc
IMS <inisoetan2014@gmail.com>
2022-08-30 12:37:30 +0100

    Adding dark mode to all pages

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 
 const container = $(".main-container")
 const icon = $("#dark-mode")
+
 icon.click(()=>{
     container.toggleClass("dark");
     if (container.hasClass("dark")){
         icon.removeClass("bi-moon-fill")
         icon.addClass("bi-brightness-high")
+        theme = "dark"
     }
     else{
         icon.removeClass("bi-brightness-high")
         icon.addClass("bi-moon-fill")
+        theme = "light"
     }
+    localStorage.setItem("PageTheme", JSON.stringify(theme))
 })
+let GetTheme = JSON.parse(localStorage.getItem("PageTheme"))
+if (GetTheme === "dark"){
+    container.addClass("dark")
+} 
